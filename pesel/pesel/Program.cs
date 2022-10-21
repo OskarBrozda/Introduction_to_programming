9a6a235a8f0d2d61e2a311c99cff61a5481dc806
Oskar Brózda <oskarbrozdaa@gmail.com>
2022-10-21 19:24:12 +0200

    it works

--- pesel/pesel/Program.cs
+++ pesel/pesel/Program.cs
@@ -4,128 +4,228 @@ class pesel
 {
     static void Main(string[] arg)
     {
-
-        //przywitanie użytkownika
-        Console.WriteLine("Cześć, jak się nazywasz (imię i nazwisko)?");
+        //przywitanie użytkownika i deklaracja zmiennych wykorzystanych potem w do...while
+        Console.WriteLine("Dzień dobry, jak się nazywasz (imię i nazwisko)?");
         var id = Console.ReadLine();
         var dane = id.Trim().Split(" ");
-
-        //prośba o numer pesel
-        Console.Write("Witaj " + dane[0]  + ", podaj swój numer pesel: ");
-        Int64 Pesel = Int64.Parse(Console.ReadLine());
-
-        //wyodrebnienie każdej z cyfr nr pesel
-        Int64 L1 = (Pesel / 10000000000) % 10;
-        Int64 L2 = (Pesel / 1000000000) % 10;
-        Int64 L3 = (Pesel / 100000000) % 10;
-        Int64 L4 = (Pesel / 10000000) % 10;
-        Int64 L5 = (Pesel / 1000000) % 10;
-        Int64 L6 = (Pesel / 100000) % 10;
-        Int64 L7 = (Pesel / 10000) % 10;
-        Int64 L8 = (Pesel / 1000) % 10;
-        Int64 L9 = (Pesel / 100) % 10;
-        Int64 L10 = (Pesel / 10) % 10;
-        Int64 L11 = Pesel % 10;
-
-        //informacje o dacie urodzenia
-        Int64 day = L5 * 10 + L6;
+        Int64 Pesel;
+        Int64 L1;
+        Int64 L2;
+        Int64 L3;
+        Int64 L4;
+        Int64 L5;
+        Int64 L6;
+        Int64 L7;
+        Int64 L8;
+        Int64 L9;
+        Int64 L10;
+        Int64 L11;
+        Int64 day;
         Int64 month;
         string monthstr;
-        if (L1 * 10 + L2 > 22)
-        {
-            month = L3 * 10 + L4;
-            monthstr = string.Format("{0:00}", month);
-
-        }
-        else
-        {
-            month = L3 * 10 + L4 - 20;
-            monthstr = string.Format("{0:00}", month);
-        }
         Int64 year;
         string yearstr;
-        string yearstr2;
-        if(L1 * 10 + L2 > 22)
-        {
-            year = L1 * 10 + L2;
-            yearstr = string.Format("{0:00}", year);
-            yearstr2 = ("19" + yearstr);
-        }
-        else
-        {
-            year = L1 * 10 + L2;
-            yearstr = string.Format("{0:00}", year);
-            yearstr2 = ("20" + yearstr);
-        }
-                    
-        //informacja o płci
-        Int64 Sex = (Pesel / 10) % 10;
+        Int64 Sex;
         string Sex2;
-        if (Sex % 2 == 0)
-        {
-            Sex2 = "kobieta";
-        }
-        else
-        {
-            Sex2 = "mężczyzna";
-        }
-      
-        //rozdzielenie nr pesel na czynniki
-        //suma kontorlna
-        if (L2 * 3 >= 10)
-        {
-            L2 = (L2 * 3) % 10;
-        }
-        if (L3 * 7 >= 10)
-        {
-            L3 = (L3 * 7) % 10;
-        }
-        if (L4 * 9 >= 10)
-        {
-            L4 = (L4 * 9) % 10;
-        }
-        if (L6 * 3 >= 10)
-        {
-            L6 = (L6 * 3) % 10;
-        }
-        if (L7 * 7 >= 10)
-        {
-            L7 = (L7 * 7) % 10;
-        }
-        if (L8 * 9 >= 10)
-        {
-            L8 = (L8 * 9) % 10;
-        }
-        if (L10 * 3 >= 10)
-        {
-            L10 = (L10 * 3) % 10;
-        }
-
-        Console.WriteLine(L1 + " " + L2 + " " + L3 + " " + L4 + " " + L5 + " " + L6 + " " + L7 + " " + L8 + " " + L9 + " " + L10);
-
-        //sytuacja gdzie suma kontrolna wychodzi 10-0=10 -> wpisujemy 0
         double Controlsum;
-        if (((L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 + L10) % 10) == 0)
+
+
+        do
         {
-            Controlsum = 0;
+            //prośba o numer pesel
+            Console.Write("Witaj " + dane[0] + ", podaj swój numer pesel: ");
+            Pesel = Int64.Parse(Console.ReadLine());
+
+            //wyodrebnienie każdej z cyfr nr pesel
+            L1 = (Pesel / 10000000000) % 10;
+            L2 = (Pesel / 1000000000) % 10;
+            L3 = (Pesel / 100000000) % 10;
+            L4 = (Pesel / 10000000) % 10;
+            L5 = (Pesel / 1000000) % 10;
+            L6 = (Pesel / 100000) % 10;
+            L7 = (Pesel / 10000) % 10;
+            L8 = (Pesel / 1000) % 10;
+            L9 = (Pesel / 100) % 10;
+            L10 = (Pesel / 10) % 10;
+            L11 = Pesel % 10;
+
+            //informacje o dacie urodzenia
+            day = L5 * 10 + L6;
+            if (L1 * 10 + L2 > 22)
+            {
+                month = L3 * 10 + L4;
+                monthstr = string.Format("{0:00}", month);
+
+            }
+            else
+            {
+                month = L3 * 10 + L4 - 20;
+                monthstr = string.Format("{0:00}", month);
+            }
+
+            if (L1 * 10 + L2 > 22)
+            {
+                year = L1 * 10 + L2;
+                yearstr = string.Format("{0:00}", year + 1900);
+            }
+            else
+            {
+                year = L1 * 10 + L2;
+                yearstr = string.Format("{0:00}", year + 2000);
+            }
+
+            //informacja o płci
+            Sex = (Pesel / 10) % 10;
+            if (Sex % 2 == 0)
+            {
+                Sex2 = "kobieta";
+            }
+            else
+            {
+                Sex2 = "mężczyzna";
+            }
+
+            //rozdzielenie nr pesel na czynniki
+            //suma kontorlna
+            if (L2 * 3 >= 10)
+            {
+                L2 = (L2 * 3) % 10;
+            }
+            else
+            {
+                L2 = L2 * 3;
+            }
+            if (L3 * 7 >= 10)
+            {
+                L3 = (L3 * 7) % 10;
+            }
+            else
+            {
+                L3 = L3 * 7;
+            }
+            if (L4 * 9 >= 10)
+            {
+                L4 = (L4 * 9) % 10;
+            }
+            else
+            {
+                L4 = L4 * 9;
+            }
+            if (L6 * 3 >= 10)
+            {
+                L6 = (L6 * 3) % 10;
+            }
+            else
+            {
+                L6 = L6 * 3;
+            }
+            if (L7 * 7 >= 10)
+            {
+                L7 = (L7 * 7) % 10;
+            }
+            else
+            {
+                L7 = L7 * 7;
+            }
+            if (L8 * 9 >= 10)
+            {
+                L8 = (L8 * 9) % 10;
+            }
+            else
+            {
+                L8 = L8 * 9;
+            }
+            if (L10 * 3 >= 10)
+            {
+                L10 = (L10 * 3) % 10;
+            }
+            else
+            {
+                L10 = L10 * 3;
+
+            }
+
+
+            //sytuacja gdzie suma kontrolna wychodzi 10-0=10 -> wpisujemy 0
+            if (((L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 + L10) % 10) == 0)
+            {
+                Controlsum = 0;
+            }
+            else
+            {
+                Controlsum = 10 - ((L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 + L10) % 10);
+            }
+        } while (L11 != Controlsum);
+        
+           
+
+        //PROGRAM DO OBLOCZANIA WIEKU DO EMERYTURY
+        int retirementAge;
+        string years;
+        int wiek;
+        if (L1 * 10 + L2 > 22)
+        {
+            wiek = (int)(2022 - (L1 * 10 + L2 + 1900));
         }
         else
         {
-            Controlsum = 10 - ((L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 + L10) % 10);
+            wiek = (int)(2022 - (L1 * 10 + L2 + 2000));
         }
 
-        //jeśli wpisano błędny pesel
-        if (L11 != Controlsum)
+        //deklaruje wiek emerytalny zależny od płci
+        if (Sex2 == "kobieta")
         {
-            Console.WriteLine("Błędny numer pesel. Podaj ponownie.");
+            retirementAge = 60;
+        }
+        else
+        {
+            retirementAge = 65;
         }
 
-        //info dla użytkownika
-        Console.WriteLine();
-        Console.WriteLine(dane[1] + ": ");
-        Console.WriteLine("Data urodzenia: " + day + "." + monthstr + "." + yearstr2);
-        Console.WriteLine("Płeć: " + Sex2);
+        // deklaruję odmianę słowa lat poprzez wykorzystanie ostatnich wyfr wieku
+        long lastDigit = (retirementAge - wiek) % (10);
+        long SecondLastDigit = ((retirementAge - wiek) / 10) % (10);
+        if (SecondLastDigit == 1)
+        {
+            years = "lat";
+        }
+        else if ((SecondLastDigit != 1) && ((lastDigit == 2) || (lastDigit == 3) || (lastDigit == 4)))
+        {
+            years = "lata";
+        }
+        else
+        {
+            years = "lat";
+        }
 
+        if ((retirementAge - wiek) == 1 || (retirementAge - wiek) == -1)
+        {
+            years = "roku";
+        }
+
+
+        //wiadomośc końcowa dla użytkownika
+        Console.WriteLine();
+        Console.WriteLine(dane[1] +" "+ dane[0] + ": ");
+        Console.WriteLine("Data urodzenia: " + day + "." + monthstr + "." + yearstr);
+        Console.WriteLine("Płeć: " + Sex2);
+        Console.WriteLine("Suma kontrolna: ZGODNA");
+        Console.WriteLine();
+        Console.WriteLine("Ciekawostka:");
+
+        //emeryturka
+        if (wiek >= 0 && wiek < retirementAge)
+        {
+            Console.WriteLine(dane[0] + ", do emerytury brakuje Ci jeszcze " + (retirementAge - wiek) + " " + years + ".");
+        }
+        else if (wiek <= 0)
+        {
+            Console.WriteLine("Wiek nie może być ujemny!");
+        }
+        else
+        {
+            Console.WriteLine("Jesteś na emeryturze od " + (wiek - retirementAge) + " " + years + ".");
+        }
 
         Console.ReadKey();
     }
