5947b46e408ef2991c663a681ea749ffc01f1d11
Oskar Brózda <oskarbrozdaa@gmail.com>
2022-11-12 11:50:17 +0100

    Begin

--- rabat_na_loty2/Program.cs
+++ rabat_na_loty2/Program.cs
@@ -1,13 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//witamy użytkownika
+using System.Xml.Linq;
 
-class rabat
+Console.WriteLine("Witaj w systemie rezerwacji lotów online! Podaj następujące informacje:");
+Console.Write("Imię: ");
+string name;
+do
 {
-    static void Main()
+    name = Console.ReadLine();
+    if (string.IsNullOrEmpty(name))
     {
-
-    Console.WriteLine("teścik");
-
+        Console.Write("Pole nie może zostać puste. Podaj imię: ");
     }
+} while (string.IsNullOrEmpty(name));
 
-}
+Console.Write("Nazwisko: ");
+string surname;
+do
+{
+    surname = Console.ReadLine();
+    if (string.IsNullOrEmpty(surname))
+    {
+        Console.Write("Pole nie może zostać puste. Podaj nazwisko: ");
+    }
+} while (string.IsNullOrEmpty(surname));
+
+Console.Write("Data urodzenia: ");
+DateTime birthDate = new DateTime();
+DateTime todayDate = new DateTime();
+TimeSpan age = todayDate.Subtract(birthDate);
+do
+{
+    birthDate = DateTime(Console.ReadLine());
+    if (string.IsNullOrEmpty(name))
+    {
+        Console.Write("Pole nie może zostać puste. Podaj imię: ");
+    }
+} while (string.IsNullOrEmpty(name));
