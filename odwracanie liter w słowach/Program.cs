44fa7bca8803ea65b2cdbb6dd5a04cc30b212a96
Oskar Brózda <oskarbrozdaa@gmail.com>
2023-01-07 19:28:19 +0100

    reversing letters in words

---
+++ odwracanie liter w słowach/Program.cs
@@ -0,0 +1,14 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string[] words = pangram.Split(' ');
+string[] result = new string[words.Length];
+
+for (int i = 0; i < words.Length; i++)
+{
+    char[] items = words[i].ToCharArray();
+    Array.Reverse(items);
+    result[i] = new string(items);
+}
+
+string print = String.Join(" ", result);
+Console.WriteLine(print);
+
