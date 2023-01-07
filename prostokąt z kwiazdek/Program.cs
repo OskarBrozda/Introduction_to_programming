8d2e798032ac3899f001ca9eb7714adaa4039adf
Oskar Brózda <oskarbrozdaa@gmail.com>
2023-01-07 19:50:29 +0100

    rectangle of stars

---
+++ prostokąt z kwiazdek/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Podaj szerokosc: ");
+string xs = Console.ReadLine();
+Console.Write("Podaj wysokosé: ");
+string ys = Console.ReadLine();
+int x = int.Parse(xs);
+int y = int.Parse(ys);
+for (int i = 0; i < y; i++)
+{
+    for (int j = 0; j < x; j++)
+    {
+        Console.Write("*");
+
+    }
+    Console.WriteLine();
+}
